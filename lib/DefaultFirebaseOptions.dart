d2d67f4d252140a7531a5130f7c3d31ec7cc0670
Mohannd Qunoo <mohanndrami1@gmail.com>
2026-06-29 12:22:21 +0300

    new commit

--- lib/DefaultFirebaseOptions.dart
+++ lib/DefaultFirebaseOptions.dart
@@ -16,23 +16,23 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // إعدادات الويب المحدثة والمتوافقة مع كود الـ Pull الجديد
+  // إعدادات الويب المحدثة للمشروع الجديد nearestWorkspace
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyArGe4b8b3jPU59_VC9vGgeeTbwevwQUhk',
     appId: '1:644719264139:web:fd502ca45a8fbfab70ef8e',
     messagingSenderId: '644719264139',
-    projectId: 'studyspot-gaza',
-    authDomain: 'studyspot-gaza.firebaseapp.com',
-    storageBucket: 'studyspot-gaza.firebasestorage.app',
+    projectId: 'nearestWorkspace', // تغيير اسم المشروع هنا
+    authDomain: 'nearestWorkspace.firebaseapp.com', // تحديث النطاق
+    storageBucket: 'nearestWorkspace.firebasestorage.app', // تحديث المخزن
     measurementId: 'G-68Q9HWCST1',
   );
 
-  // إعدادات الأندرويد المحدثة والمتوافقة مع كود الـ Pull الجديد
+  // إعدادات الأندرويد المحدثة للمشروع الجديد nearestWorkspace
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDFjmELB0z48hIh9ndM63a9VzSJ51ANoB8',
     appId: '1:644719264139:android:b72ef29cea8f6e0d70ef8e',
     messagingSenderId: '644719264139',
-    projectId: 'studyspot-gaza',
-    storageBucket: 'studyspot-gaza.firebasestorage.app',
+    projectId: 'nearestWorkspace', // تغيير اسم المشروع هنا ليطابق قاعدة البيانات الجديدة
+    storageBucket: 'nearestWorkspace.firebasestorage.app', // تحديث المخزن للأندرويد
   );
 }
